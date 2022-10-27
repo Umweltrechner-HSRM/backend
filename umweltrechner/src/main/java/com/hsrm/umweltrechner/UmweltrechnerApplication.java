127a43896f91d8c7c72f7b41a8d22b6a8fe34752
gitchrisries <christophries1@student.hs-rm.de>
2022-10-27 18:27:08 +0200

    initial commit

---
+++ umweltrechner/src/main/java/com/hsrm/umweltrechner/UmweltrechnerApplication.java
@@ -0,0 +1,13 @@
+package com.hsrm.umweltrechner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmweltrechnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmweltrechnerApplication.class, args);
+	}
+
+}
---
+++ umweltrechner/src/test/java/com/hsrm/umweltrechner/UmweltrechnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsrm.umweltrechner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmweltrechnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

5d468587628bf7357a5041953f59097abac0ca9a
skyprime_17 <nmoeller@live.de>
2022-10-28 22:31:59 +0200

    sse endpoint test

--- umweltrechner/src/main/java/com/hsrm/umweltrechner/EnvironmentCalculatorApplication.java
+++ umweltrechner/src/main/java/com/hsrm/umweltrechner/EnvironmentCalculatorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UmweltrechnerApplication {
+public class EnvironmentCalculatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UmweltrechnerApplication.class, args);
+		SpringApplication.run(EnvironmentCalculatorApplication.class, args);
 	}
 
 }

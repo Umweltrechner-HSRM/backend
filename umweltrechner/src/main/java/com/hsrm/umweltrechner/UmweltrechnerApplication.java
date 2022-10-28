934bbb3f10e81505a4700563fa820c105a27cf92
Chris <chrisries@me.com>
2022-10-28 08:43:44 +0200

    test

--- umweltrechner/src/main/java/com/hsrm/umweltrechner/UmweltrechnerApplication.java
+++ umweltrechner/src/main/java/com/hsrm/umweltrechner/UmweltrechnerApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UmweltrechnerApplication {
 
+	public void test(){
+
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(UmweltrechnerApplication.class, args);
 	}

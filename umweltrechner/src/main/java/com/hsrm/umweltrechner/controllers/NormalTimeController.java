e6326f0c72ec693493c53546da9ce4091702e438
skyprime_17 <nmoeller@live.de>
2022-10-29 02:06:02 +0200

    normal rest endpoint and scheduler to generate random data

---
+++ umweltrechner/src/main/java/com/hsrm/umweltrechner/controllers/NormalTimeController.java
@@ -0,0 +1,43 @@
+package com.hsrm.umweltrechner.controllers;
+
+import java.util.Date;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@EnableScheduling
+public class NormalTimeController {
+
+  @Data
+  public static class TemperatureDto {
+    private Integer temperature;
+  }
+
+  private final ConcurrentLinkedQueue<TemperatureDto> queue = new ConcurrentLinkedQueue<>();
+
+  @CrossOrigin
+  @GetMapping("/temperature")
+  public TemperatureDto getLatestTime() {
+    return queue.peek();
+  }
+
+  @Scheduled(fixedRate = 5000)
+  public void addTime() {
+    queue.poll();
+    TemperatureDto temperatureDto = new TemperatureDto();
+    temperatureDto.setTemperature((int) (Math.random() * 100));
+    queue.add(temperatureDto);
+  }
+
+
+
+}
--- umweltrechner/src/main/java/com/hsrm/umweltrechner/controllers/SseTimeController.java
+++ umweltrechner/src/main/java/com/hsrm/umweltrechner/controllers/SseTimeController.java
@@ -18,7 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-public class TimeController {
+public class SseTimeController {
 
   private final ExecutorService executor = Executors.newSingleThreadExecutor();
 

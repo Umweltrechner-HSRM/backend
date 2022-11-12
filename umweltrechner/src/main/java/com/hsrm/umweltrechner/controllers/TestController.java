0304557ff6f9da8da0c6655fd67842a8b58ae953
skyprime_17 <nmoeller@live.de>
2022-11-12 14:45:52 +0100

    - xd

--- umweltrechner/src/main/java/com/hsrm/umweltrechner/controllers/TestController.java
+++ umweltrechner/src/main/java/com/hsrm/umweltrechner/controllers/TestController.java
@@ -6,19 +6,23 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/transactions")
-@Slf4j
-@RequiredArgsConstructor
+@RequestMapping("/v1/test")
 public class TestController {
 
-  @GetMapping(value = "/findAll", produces = MediaType.APPLICATION_JSON_VALUE)
-  @RolesAllowed("admin")
-  public ResponseEntity<String> hello(){
-    return ResponseEntity.ok("hello");
+  @GetMapping(value = "/secret", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PreAuthorize("hasRole('admin')")
+  public ResponseEntity<String> xsecret(){
+    return ResponseEntity.ok("secret");
+  }
+
+  @GetMapping(value = "/public", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<String> xpublic(){
+    return ResponseEntity.ok("public");
   }
 }

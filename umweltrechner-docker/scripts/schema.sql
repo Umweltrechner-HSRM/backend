a77c2f7478a2421a79bae8a8a5e32b2917cec9ea
Nurhak Tuekek <nurhak.tuekek@c24.de>
2022-11-27 15:46:15 +0100

    Database setup

--- umweltrechner-docker/scripts/schema.sql
+++ umweltrechner-docker/scripts/schema.sql
@@ -1,1 +1,20 @@
 CREATE DATABASE IF NOT EXISTS umweltrechner;
+
+create table if not exists umweltrechner.formula
+(
+  id      varchar(36)   not null
+  primary key,
+  formula varchar(1000) null
+);
+
+create table if not exists umweltrechner.sensor
+(
+  name            varchar(100)  not null
+  primary key,
+  location        varchar(100)  null,
+  description     varchar(500)  null,
+  value           double        null,
+  unit            varchar(36)   null,
+  simulation_code varchar(1000) null,
+  created_at      timestamp     null
+  );

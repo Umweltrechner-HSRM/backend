ae92daffe200d239455ebacc2eca4617d48cebb6
Niklas <116726910+nhett@users.noreply.github.com>
2022-12-14 17:49:28 +0100

    Delete InvalidSymbolException.java

--- umweltrechner/src/main/java/com/hsrm/umweltrechner/syntax/InvalidSymbolException.java
+++
@@ -1,7 +0,0 @@
-package com.hsrm.umweltrechner.syntax;
-
-public class InvalidSymbolException extends InterpreterException {
-    public InvalidSymbolException(String sym) {
-        super("Invalid symbol " + sym);
-    }
-}

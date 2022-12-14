54ca0fd4a3b5603e3f350e95fd4869e252c91c93
Niklas <116726910+nhett@users.noreply.github.com>
2022-12-14 17:48:39 +0100

    Delete DivideByZeroException.java

--- umweltrechner/src/main/java/com/hsrm/umweltrechner/syntax/DivideByZeroException.java
+++
@@ -1,7 +0,0 @@
-package com.hsrm.umweltrechner.syntax;
-
-public class DivideByZeroException extends InterpreterException {
-    public DivideByZeroException(int line, int index) {
-        super("Divide by zero at line " + (line + 1) + " and character " + (index + 1));
-    }
-}

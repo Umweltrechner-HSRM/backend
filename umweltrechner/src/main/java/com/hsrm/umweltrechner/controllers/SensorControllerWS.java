58dd2c3a4bb6ec2b176e6d676c78743b789167c3
skyprime_17 <nmoeller@live.de>
2023-01-10 01:13:39 +0100

    - only send new values, dont send values when no new timestamp

--- umweltrechner/src/main/java/com/hsrm/umweltrechner/controllers/SensorControllerWS.java
+++ umweltrechner/src/main/java/com/hsrm/umweltrechner/controllers/SensorControllerWS.java
@@ -1,6 +1,8 @@
 package com.hsrm.umweltrechner.controllers;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
